a774592f148c5ca9e747df7c75330460d1e74816
avinash6302 <168989690+avinash6302@users.noreply.github.com>
2024-05-06 08:21:27 +0530

    Update conv.py

--- Wav2Lip/models/conv.py
+++ Wav2Lip/models/conv.py
@@ -6,11 +6,12 @@ class Conv2d(nn.Module):
     def __init__(self, cin, cout, kernel_size, stride, padding, residual=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conv_block = nn.Sequential(
-            nn.Conv2d(cin, cout, kernel_size, stride, padding),
-            nn.BatchNorm2d(cout)
-        )
-        self.act = nn.Relu()
+                            nn.Conv2d(cin, cout, kernel_size, stride, padding),
+                            nn.BatchNorm2d(cout)
+                            )
+        self.act = nn.ReLU()
         self.residual = residual
+
     def forward(self, x):
         out = self.conv_block(x)
         if self.residual:
@@ -21,21 +22,23 @@ class nonorm_Conv2d(nn.Module):
     def __init__(self, cin, cout, kernel_size, stride, padding, residual=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conv_block = nn.Sequential(
-            nn.Conv2d(cin, cout, kernel_size, stride, padding),
-        )
+                            nn.Conv2d(cin, cout, kernel_size, stride, padding),
+                            )
         self.act = nn.LeakyReLU(0.01, inplace=True)
+
     def forward(self, x):
         out = self.conv_block(x)
         return self.act(out)
 
 class Conv2dTranspose(nn.Module):
-    def __init__(self, cin, cout, kernel_size, stride, padding, *args, **kwargs):
+    def __init__(self, cin, cout, kernel_size, stride, padding, output_padding=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conv_block = nn.Sequential(
-            nn.Conv2d(cin, cout, kernel_size, stride, padding),
-            nn.BatchNorm2d(cout)
-        )
-        self.act = nn.Relu()
+                            nn.ConvTranspose2d(cin, cout, kernel_size, stride, padding, output_padding),
+                            nn.BatchNorm2d(cout)
+                            )
+        self.act = nn.ReLU()
+
     def forward(self, x):
         out = self.conv_block(x)
         return self.act(out)
